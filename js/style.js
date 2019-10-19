0c56eb934cbc169c310926d82be58033f4e7a8d3
Moetech13 <Moetechtainment@gmail.com>
2019-10-18 23:20:17 -0400

    preloader

--- js/style.js
+++ js/style.js
@@ -1,5 +1,12 @@
-// IFRAME
-// VanillaJS version
+// PRELOADER
+//<![CDATA[
+  $(window).load(function() { // makes sure the whole site is loaded
+		$("#status").fadeOut(); // will first fade out the loading animation
+		$("#preloader").delay(350).fadeOut("slow"); // will fade out the white DIV that covers the website.
+	})
+//]]>
+
+// IFRAME VanillaJS version
 
 function iframeAutoScale() {
   
